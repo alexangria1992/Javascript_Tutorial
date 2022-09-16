4331533cd5568b002572bbf2315b9c24f2cb9871
alexangria1992 <alex040892@gmail.com>
2022-09-16 20:07:50 +1200

    Numbers

--- playground/types.js
+++ playground/types.js
@@ -1,25 +1,39 @@
-/* eslint-disable */
-const name = "alex";
-const middle = "topher";
-const last = `bos`;
+// /* eslint-disable */
+// const name = "alex";
+// const middle = "topher";
+// const last = `bos`;
 
-const sentence = "she's so cool";
+// const sentence = "she's so cool";
 
-// const hello = "hello my name is " + name + " . Nice to meet you";
+// // const hello = "hello my name is " + name + " . Nice to meet you";
 
-// let hello2 = "hello my name is ";
-// hello2 = hello2 + name;
-// hello2 = hello2 + " Nice to meet you";
+// // let hello2 = "hello my name is ";
+// // hello2 = hello2 + name;
+// // hello2 = hello2 + " Nice to meet you";
 
-const hello = `hello my name is ${name}. Nice to meet you. I am ${
-  1 + 100
-} years old`;
+// const hello = `hello my name is ${name}. Nice to meet you. I am ${
+//   1 + 100
+// } years old`;
 
-const html = ` 
-    <div>
-        <h2>${name}</h2>
-        <p>${hello}</p>
-    </div>
-`;
+// const html = `
+//     <div>
+//         <h2>${name}</h2>
+//         <p>${hello}</p>
+//     </div>
+// `;
 
-document.body.innerHTML = html;
+// document.body.innerHTML = html;
+
+const age = 100.5;
+const name = "wes";
+
+// const b = 20;
+// const a = 10;
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+const dadGets = smarties % kids;
+console.log(`each kids get ${eachKidGets}`);
+
+const price = 1034;
