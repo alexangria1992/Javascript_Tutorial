4dc1cc8dc8cde291152ac02b52839e31f3a44b5e
alexangria1992 <alex040892@gmail.com>
2022-09-17 17:02:51 +1200

    Custom Functions

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,11 @@
+// Function Definition
+
+function calculateBill() {
+  // this is the function body
+  console.log("Running Calculate Bill");
+  const total = 100 * 1.13;
+  return total;
+}
+
+const myTotal = calculateBill();
+console.log(`your total is $${calculateBill()}`);
