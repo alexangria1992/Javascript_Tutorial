35992206ac32378b10d23d9c0db997d48f5c0dcf
alexangria1992 <alex040892@gmail.com>
2022-09-17 17:53:02 +1200

    Parameters and Arguments

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,11 +1,33 @@
-// Function Definition
+// prettier-ignore
 
-function calculateBill() {
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
   // this is the function body
-  console.log("Running Calculate Bill");
-  const total = 100 * 1.13;
+//   console.log(billAmount, taxRate);
+  console.log('Running Calculate Bill');
+  const total = billAmount  + billAmount *  taxRate + billAmount * tipRate;
   return total;
 }
 
-const myTotal = calculateBill();
-console.log(`your total is $${calculateBill()}`);
+const alexTotal = 500;
+const alexTaxRate = 0.3;
+// const myTotal = calculateBill(alexTotal, alexTaxRate);
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+// const greeting = sayHiTo('Alex');
+// console.log(greeting);
+
+// const myTotal3 = calculateBill(20 + 20 + 30 + 20, 0.15);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name = 'Silly Goose') {
+  return `HEY ${name.toUpperCase()}`;
+}
+
+const myBill4 = calculateBill(100, undefined, 0.2);
+console.log(myBill4);
