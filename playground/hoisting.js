c59d62df2f2e4743f5e1dff3e41c3b2fdf27520d
alexangria1992 <alex040892@gmail.com>
2022-09-26 15:12:06 +1300

    hoisting

---
+++ playground/hoisting.js
@@ -0,0 +1,17 @@
+// prettier-ignore
+/* eslint-disable */
+
+var age;
+console.log(age);
+age = 10;
+
+sayHi();
+
+function sayHi() {
+  console.log('Hey');
+  console.log(add(10,2));
+}
+
+function add(a,b) {
+    return a + b;
+}
