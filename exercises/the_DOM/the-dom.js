4d5106e25f2183cbc46a6d911be78d246ca5228e
alexangria1992 <alex040892@gmail.com>
2022-09-26 18:37:37 +1300

    DOM-Working with classes

--- exercises/the_DOM/the-dom.js
+++ exercises/the_DOM/the-dom.js
@@ -1,22 +1,36 @@
 // prettier-ignore
 /* eslint-disable */
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const item2 = document.querySelector('.item2');
-const item2Image = item2.querySelector('img');
-const heading = document.querySelector('h2');
-console.log(heading.textContent);
-console.log(heading.innerText);
-// heading.textContent = "Alex is cool"
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const item2 = document.querySelector('.item2');
+// const item2Image = item2.querySelector('img');
+// const heading = document.querySelector('h2');
 // console.log(heading.textContent);
 // console.log(heading.innerText);
-console.log(heading.innerHTML);
-console.log(heading.outerHTML);
+// // heading.textContent = "Alex is cool"
+// // console.log(heading.textContent);
+// // console.log(heading.innerText);
+// console.log(heading.innerHTML);
+// console.log(heading.outerHTML);
 
-const pizzaList = document.querySelector('.pizza')
-console.log(pizzaList.textContent);
-// pizzaList.textContent = `${pizzaList.textContent} 🍕` ;
+// const pizzaList = document.querySelector('.pizza')
+// console.log(pizzaList.textContent);
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕` ;
 
-pizzaList.insertAdjacentText('afterbegin', +'🍕' );
-pizzaList.insertAdjacentText('beforeend', +'🍕' );
+// pizzaList.insertAdjacentText('afterbegin', +'🍕' );
+// pizzaList.insertAdjacentText('beforeend', +'🍕' );
 
+// CLASSES! 
+
+const pic = document.querySelector('.nice');
+pic.classList.add('open')
+pic.classList.remove('cool')
+
+console.log(pic.classList);
+
+function toggleRound(){
+    pic.classList.toggle("round");
+
+}
+
+pic.addEventListener('click', toggleRound)
